c422a3cae32d072d87e5b8235541975b39331bb8
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2018-01-20 08:36:04 +0530

    Added local storage

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,32 +6,50 @@ export class DataService {
   questions: Question[];
 
   constructor() {
-    this.questions = [
-      {
-        text: 'What is your name?',
-        answer: 'Sanket Singh',
-        hide: true
-      },
-      {
-        text: 'What you like the most?',
-        answer: 'Competitive Programming',
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     text: 'What is your name?',
+    //     answer: 'Sanket Singh',
+    //     hide: true
+    //   },
+    //   {
+    //     text: 'What you like the most?',
+    //     answer: 'Competitive Programming',
+    //     hide: true
+    //   }
+    // ];
   }
 
   getQuestions() {
+    if (localStorage.getItem('questions') === null) {
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
   }
 
   addQuestion(question: Question) {
     this.questions.unshift(question);
+    let questions;
+    if (localStorage.getItem('questions') === null) {
+      questions = [];
+      // push new ques
+      questions.unshift(question);
+      // set to local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+      questions.unshift(question);
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
   }
 
   removeQuestion(question: Question) {
     for (let i = 0; i < this.questions.length; i++) {
       if (question === this.questions[i]) {
         this.questions.splice(i, 1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
       }
     }
   }
